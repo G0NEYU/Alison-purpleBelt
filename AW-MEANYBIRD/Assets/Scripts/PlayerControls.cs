881abf9e7a238c86efe4269dbe87d7e90c275a7b
Aliso n <bunbunsoswag@gmail.com>
2022-08-07 13:48:35 -0400

    meanybirds step 11

---
+++ AW-MEANYBIRD/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // game manager object
+    [Header("Game Controller Object for controlling the game")]
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
