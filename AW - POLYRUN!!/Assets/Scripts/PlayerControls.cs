dffa4423487cc4e251d4de71388aab334682bc9b
alison <bunbunsoswag@gmail.com>
2022-09-25 13:52:42 -0400

    step 39

--- AW - POLYRUN!!/Assets/Scripts/PlayerControls.cs
+++ AW - POLYRUN!!/Assets/Scripts/PlayerControls.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    [Header("Default Jumping Power")]
+    public float jumpPower = 6f;
+
+    [Header("Booleen isGrounded")]
+    public bool isGrounded = false;
+
+    float posX = 0.0f;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = transform.GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    void FixedUpdate()
+    {
+        {
+            if (Input.GetKey(KeyCode.Space) && isGrounded)
+            {
+                rb.AddForce(Vector3.up * (jumpPower * rb.mass * rb.gravityScale * 20.0f));
+            }
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground") ;
+            isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground") ;
+        isGrounded = false;
     }
 }
--- AW - POLYRUN!!/Assets/Scripts/Spawner.cs
+++ AW - POLYRUN!!/Assets/Scripts/Spawner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
-{
+{   [Header("ChallengeObj Game Object")]
     // Start is called before the first frame update
     void Start()
     {
