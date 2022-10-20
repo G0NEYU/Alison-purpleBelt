24753f83b9538c50f7e2e5a645523d1fefa5dc7d
alison <bunbunsoswag@gmail.com>
2022-10-20 18:39:49 -0400

    pv stack

--- stack xoxo/Assets/GameController.cs
+++ stack xoxo/Assets/GameController.cs
@@ -33,17 +33,18 @@ public class GameController : MonoBehaviour
         }
         var time = Mathf.Abs(Time.realtimeSinceStartup % 2f - 1f);
         var pos1 = lastCube.transform.position + Vector3.up * 10f;
-        var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
-        var pos3 = pos1 + ((Level % 2== 0) ? Vector3.right : Vector3.back) * 240;
+        var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120; 
+        var pos3 = pos2 + (Level % 2 == 0  ? Vector3.right : Vector3.back) * 200;
+       
 
 
         if (Level % 2 == 0)
         {
-            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+            currentCube.transform.position = Vector3.Lerp( pos2, pos3 ,time);
 
         } else
         {
-            currentCube.transform.position = Vector3.Lerp(pos1, pos2, time);
+            currentCube.transform.position = Vector3.Lerp(pos2 ,pos3 , time);
         } 
         if (Input.GetMouseButtonDown(0))
         { 
