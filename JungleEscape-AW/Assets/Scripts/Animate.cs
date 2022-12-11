bba8bf688136a3372882d81323bdbda85039feb7
Alison <bunbunsoswag@gmail.com>
2022-12-11 13:51:01 -0500

    stepp 11

---
+++ JungleEscape-AW/Assets/Scripts/Animate.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+    // Start is called before the first frame update 
+    Animator Animator;
+    Jump Jump;
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+        Jump = GetComponent<Jump>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Jump.isGrounded)
+        {
+           // Animator.SetBool("isJumping, true");
+            //Animator.SetBool("isIdle, false");
+           // Anima
+        } /
+        if (Jump.isGrounded)
+        {
+            if (Input.GetAxisRaw("Vertical") ==1)
+            {
+
+            } 
+            if(Input.GetAxisRaw("Vertical") ==-1)
+            {
+
+            }
+        }
+    }
+}
---
+++ JungleEscape-AW/Assets/Scripts/Jump.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    float jumpForce = 5.7f;
+   bool canJump;
+    public bool isGrounded;
+    float fallMultiplier = 1.5f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update() 
+        
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, .15f);
+        Debug.DrawRay(transform.position, Vector3.down * .15f, Color.red);
+        if (rigidbody.velocity.y > -.01 && rigidbody.velocity.y < .01)
+        {
+            canJump = true;
+            
+        } else
+        {
+            canJump = false;
+        }
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        } 
+        if (rigidbody.velocity.y < 0)
+        {
+            rigidbody.velocity += Physics.gravity * fallMultiplier * Time.deltaTime;
+        }
+    }
+}
+
