60e76bd016981e5de8fba776adb3e441d64b98d7
Alison <bunbunsoswag@gmail.com>
2022-12-18 13:51:08 -0500

    pv

--- JungleEscapePY-AW/Assets/Scripts/DetectFalsePlatforms.cs
+++ JungleEscapePY-AW/Assets/Scripts/DetectFalsePlatforms.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class DetectFalsePlatforms : MonoBehaviour
 {
-
+    public bool hit;
     void Update()
     {
-       
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        int layerMask = 1 << 8;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2f , layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Careful ahead");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Clear ");
+
+        }
+
     }
 }
