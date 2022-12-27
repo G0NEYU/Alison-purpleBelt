0fe702f7e263f4e7e1a2d56c2c9926b00dd73c04
Alison <bunbunsoswag@gmail.com>
2022-12-27 15:26:49 -0500

    8c-NinjaRun

---
+++ NinjaRun-AW/Assets/Scripts/Pickups.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    public ParticleSystem Pickups;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {  
+            score++;
+            scoreText.text = score.ToString();
+            Destroy(other.gameObject);
+
+            Pickups.Play();
+
+        }
+
+    }
+
+
+        void Start()
+        {
+        Pickups.Stop();
+        }
+
+        void Update()
+        {
+
+        }
+    }
