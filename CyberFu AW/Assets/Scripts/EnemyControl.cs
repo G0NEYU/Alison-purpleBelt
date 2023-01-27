3204d4e32790f317aa7e2f8af1ac33269ee39062
alisqon <bunbunsoswag@gmail.com>
2023-01-27 17:58:59 -0500

    CyberFu- 21d

--- CyberFu AW/Assets/Scripts/EnemyControl.cs
+++ CyberFu AW/Assets/Scripts/EnemyControl.cs
@@ -8,10 +8,10 @@ public class EnemyControl : MonoBehaviour
     public float speed = 5;
     public float attackingDistance = 1;
     public Vector3 direction;
-    private bool isFollowingTarget;
-    private bool isAttackingTarget;
+    public bool isFollowingTarget;
+    public bool isAttackingTarget;
     private float chasingPlayer = 0.01f;
-    private float currentAttackingTime;
+    public float currentAttackingTime;
     private float maxAttackingTime = 2f;
 
     private Animator  animatorEnemy;
@@ -25,16 +25,79 @@ public class EnemyControl : MonoBehaviour
         rigidbodyEnemy = GetComponent<Rigidbody>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
 
+      
+    }
+    void FollowTarget()
+    {
         if (!isFollowingTarget)
         {
             return;
         }
 
+        if (Vector3.Distance(transform.position, target.position) >=
+            attackingDistance)
+        {
+            direction = target.position - transform.position;
+            direction.y = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 100);
+            if (rigidbodyEnemy.velocity.sqrMagnitude != 0)
+            {
+                rigidbodyEnemy.velocity = transform.forward * speed;
+                animatorEnemy.SetBool("Walk", true);
+            }
+        } 
+        else if(Vector3.Distance(transform.position, 
+            target.position) <= attackingDistance)
+        {
+            rigidbodyEnemy.velocity = Vector3.zero;
+            animatorEnemy.SetBool("Walk", false);
+            isFollowingTarget = false;
+            isAttackingTarget = true;
+
+         
+        }
+        if (Vector3.Distance(transform.position, target.position) >
+            attackingDistance + chasingPlayer)
+        {
+            isAttackingTarget = false;
+            isFollowingTarget = true;
+        }
+
+       
+    }
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        FollowTarget();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Attack()
     {
-        
+        if (!isAttackingTarget)
+        {
+            return;
+        }
+        currentAttackingTime += Time.deltaTime;
+
+        if (currentAttackingTime > maxAttackingTime)
+        {
+            currentAttackingTime = 0f;
+            animatorEnemy.SetTrigger("Attack1");
+        } 
+
     }
+
+    private void Update()
+    {
+        Attack();
+    } 
+
+   public void EnemyAttack(int attack)
+    {
+        if (attack ==1 )
+        {
+            animatorEnemy.SetTrigger("Attack1");
+        }
+    } 
 }
+
