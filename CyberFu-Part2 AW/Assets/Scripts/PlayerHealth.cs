875e8f15c1d2b31c8d341d3e1024ef2383469c93
Alison <bunbunsoswag@gmail.com>
2023-02-28 20:01:41 -0500

    CyberFu2-Pv

--- CyberFu-Part2 AW/Assets/Scripts/PlayerHealth.cs
+++ CyberFu-Part2 AW/Assets/Scripts/PlayerHealth.cs
@@ -6,10 +6,44 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     public int currentPlayerHealth = 10;
+    public int enemyDamage = 2;
+
+    public PlayerExplosionParticles particles;
+
+    private Animator playerAnimator;
    
     void Start()
     {
-       
+        playerAnimator = GetComponent<Animator>();
+        particles = GetComponent<PlayerExplosionParticles>();
     }
-    
+     
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage; 
+
+        if (currentPlayerHealth <= 0 )
+        {
+            gameObject.SetActive(false);
+        } 
+
+        if (currentPlayerHealth <= 0)
+        {
+            // gameObject.SetActive(false);
+            particles.Explode();
+            Invoke("ReloadScene", 5);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "HitCollider")
+        {
+            HurtPlayer();
+            playerAnimator.SetTrigger("Hit");
+        }
+    }  
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("CyberFu");
+    }
 }
