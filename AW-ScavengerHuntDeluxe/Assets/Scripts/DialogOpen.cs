dae82ae77692947b2c5c89f25fd3712416431d3a
Alison <bunbunsoswag@gmail.com>
2023-04-02 16:01:00 -0400

    ScavengerHuntDeluxe-PV

--- AW-ScavengerHuntDeluxe/Assets/Scripts/DialogOpen.cs
+++ AW-ScavengerHuntDeluxe/Assets/Scripts/DialogOpen.cs
@@ -35,7 +35,7 @@ public class DialogOpen : MonoBehaviour
         {
             checkClue();
         }
-        greeting.Play(0);
+        greeting.Play();
         interfaceManager.GetComponent<InterfaceManager>().ShowBox(dialog, clue);
     }
 
@@ -59,6 +59,47 @@ public class DialogOpen : MonoBehaviour
 
     public void searchDialog()
     {
-        dialog = "Hi! Can you help me find my " + collectibles[clue] + "?";
+       if (clue == 0)
+        {
+            dialog = "Hi! Cant miss this movie tonight, find my film?" ;
+        }
+       if (clue ==1)
+        {
+            dialog = "Hi! I need my balloons for this party, can you find them?";
+        }
+       if (clue ==2)
+        {
+            dialog = "Hi! i need my life saver to go to the beach , help me find it?" ;
+        }
+       if (clue ==3 )
+        {
+            dialog = "Hi! i lost my bull's eye can you find it?" ;
+        }
+       if (clue ==4)
+        {
+            dialog = "Hi! i lost my friends pipe,can you help me find it?";
+        }
+        if(clue ==5)
+        {
+            dialog = "Hi i lost the key to my house, help me find it?";
+
+        }
+        if (clue == 6)
+        {
+            dialog = "Hi! i lost my pet fish, can you find it?";
+        }
+        if(clue==7)
+        {
+            dialog = "Hi! i lost my birdhouse, help me find it?";
+        }
+        if (clue == 8)
+        {
+            dialog = "Hi! i lost my favorite red airhorn,can you help me find it?";
+        }
+        if (clue==9)
+        {
+            dialog = "Hi! i need my hat for my magic show! Can you help me find it?";
+        }
+       // dialog = "Hi! can you help me find my " + collectibles[clue] + "?";
     }
 }
